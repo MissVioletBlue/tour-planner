52a997daa6a929ea201fe49b04793a262e0b2376
if23b266 <if23b266@technikum-wien.at>
2025-03-01 11:12:14 +0100

    Revamped project structure

---
+++ Swen2Project/TourPlanner.ViewModels/Commands/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+
+namespace Swen2Project.TourPlanner.ViewModels.Commands;
+
+public class RelayCommand : ICommand
+{
+    public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+    {
+        ExecuteCommand = execute;
+        CanExecuteCommand = canExecute;
+    }
+    
+    private Action<object?> ExecuteCommand { get; set; }
+    private Predicate<object?> CanExecuteCommand { get; set; }
+    
+    public bool CanExecute(object? parameter)
+    {
+        return CanExecuteCommand(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        ExecuteCommand(parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
---
+++ Swen2Project/TourPlanner.ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Swen2Project.TourPlanner.Presentation.Views;
+using Swen2Project.TourPlanner.ViewModels.Commands;
+
+namespace Swen2Project.TourPlanner.ViewModels;
+
+public class MainWindowViewModel
+{
+    public MainWindowViewModel()
+    {
+        ShowAddTourCommand = new RelayCommand(ShowAddTourWindow, CanShowAddTourWindow);
+    }
+    
+    public ICommand ShowAddTourCommand { set; get; }
+    
+    private static void ShowAddTourWindow(object? obj)
+    {
+        var addTourWindow = new AddingTourWindow();
+        addTourWindow.Show();
+    }
+    
+    private static bool CanShowAddTourWindow(object obj)
+    {
+        return true;
+    }
+}
