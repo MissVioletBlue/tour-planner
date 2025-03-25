6531943cc408c63fbf68037fd7cb7276d11a9a88
if23b266 <if23b266@technikum-wien.at>
2025-03-25 17:22:37 +0100

    Added TourDetailsHeaderControl

---
+++ TourPlanner.Presentation/Presentation/Controls/TourDetailsControls/TourDetailsHeaderControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace TourPlanner.Presentation.Presentation.Controls.TourDetailsControls;
+
+public partial class TourDetailsHeaderControl : UserControl
+{
+    public TourDetailsHeaderControl()
+    {
+        InitializeComponent();
+    }
+}
