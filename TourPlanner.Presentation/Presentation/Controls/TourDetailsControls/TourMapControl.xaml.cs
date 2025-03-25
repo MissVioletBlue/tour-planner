3a369e8c566daac3e20b714bc052100190791fd5
if23b266 <if23b266@technikum-wien.at>
2025-03-25 17:23:06 +0100

    Added TourMapControl

---
+++ TourPlanner.Presentation/Presentation/Controls/TourDetailsControls/TourMapControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace TourPlanner.Presentation.Presentation.Controls.TourDetailsControls;
+
+public partial class TourMapControl : UserControl
+{
+    public TourMapControl()
+    {
+        InitializeComponent();
+    }
+}
